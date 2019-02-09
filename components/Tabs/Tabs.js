26600974b606aa71f1cb7a150388b4bb69239b17
rberriz <rberriz@icloud.com>
2019-02-09 12:02:28 -0500

    New Branch

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -64,7 +64,7 @@ class TabCard {
 
 - Select all classes named ".tab" and assign that value to the tabs variable
 
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
+- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeListt
 
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
